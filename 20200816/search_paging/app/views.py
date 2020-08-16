57fbfad87f4a082e84c5082e21ca2f8aedd00ca1
Tung Duong <duongthanhtungvn01@gmail.com>
2020-08-16 08:08:17 +0100

    Update

--- 20200816/search_paging/app/views.py
+++ 20200816/search_paging/app/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import *
+
 def index(request):
-    return render(request, 'index.html')
+    productList = Product.objects.all()
+    context = {
+        'productList':  productList
+    }
+    return render(request, 'index.html'
+                    , context)
