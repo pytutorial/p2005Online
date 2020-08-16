d88e1e2a302d49367adab70bf9b1c24db5d8ec40
Tung Duong <duongthanhtungvn01@gmail.com>
2020-08-16 08:53:06 +0100

    Update

--- 20200816/search_paging/app/views.py
+++ 20200816/search_paging/app/views.py
@@ -3,15 +3,30 @@ from django.shortcuts import render
 # Create your views here.
 from .models import *
 
+def getPriceRangeValue(priceRange):
+    if priceRange == 1: return (0, 5)
+    if priceRange == 2: return (5, 10)
+    if priceRange == 3: return (10, 20)
+    if priceRange == 4: return (20, None)
+    return (None, None)
+
 def index(request):
     categoryId = request.GET.get('categoryId', '')
     categoryId = int(categoryId) if categoryId else ''
 
     priceRange = request.GET.get('priceRange', '')
     priceRange = int(priceRange) if priceRange else ''
+    minPrice, maxPrice = getPriceRangeValue(priceRange)
 
     keyword = request.GET.get('keyword', '')
     productList = Product.objects.filter(name__contains=keyword)
+    
+    if minPrice: 
+        productList = productList.filter(price__gte=minPrice) 
+
+    if maxPrice:
+        productList = productList.filter(price__lte=maxPrice)
+
     productList = productList.filter(category__id=categoryId)
     categoryList = Category.objects.all()
     context = {
