7923cc0e120ac60475e41ed3c24015a8fba8f623
Tung Duong <duongthanhtungvn01@gmail.com>
2020-08-16 08:23:24 +0100

    Update

--- 20200816/search_paging/app/views.py
+++ 20200816/search_paging/app/views.py
@@ -6,8 +6,10 @@ from .models import *
 def index(request):
     keyword = request.GET.get('keyword', '')
     productList = Product.objects.filter(name__contains=keyword)
+    categoryList = Category.objects.all()
     context = {
         'keyword': keyword,
+        'categoryList': categoryList,
         'productList':  productList
     }
     return render(request, 'index.html' , context)
