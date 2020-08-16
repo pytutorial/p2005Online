9435d6c3696ec924663da73540cc447cbb4d1220
Tung Duong <duongthanhtungvn01@gmail.com>
2020-08-16 08:44:44 +0100

    w Update

--- 20200816/search_paging/app/views.py
+++ 20200816/search_paging/app/views.py
@@ -6,11 +6,16 @@ from .models import *
 def index(request):
     categoryId = request.GET.get('categoryId', '')
     categoryId = int(categoryId) if categoryId else ''
+
+    priceRange = request.GET.get('priceRange', '')
+    priceRange = int(priceRange) if priceRange else ''
+
     keyword = request.GET.get('keyword', '')
     productList = Product.objects.filter(name__contains=keyword)
     productList = productList.filter(category__id=categoryId)
     categoryList = Category.objects.all()
     context = {
+        'priceRange': priceRange,
         'categoryId': categoryId,
         'keyword': keyword,
         'categoryList': categoryList,
