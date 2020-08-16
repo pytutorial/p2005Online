131a3ffa0a8e0c5ed618761922f57b56ab4dc7e6
Tung Duong <duongthanhtungvn01@gmail.com>
2020-08-16 08:00:34 +0100

    Update

--- 20200816/search_paging/app/models.py
+++ 20200816/search_paging/app/models.py
@@ -3,9 +3,13 @@ from django.db import models
 class Category(models.Model):
     code = models.CharField(max_length=20, unique=True)
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
     code = models.CharField(max_length=20, unique=True)
     name = models.CharField(max_length=200) 
     price = models.FloatField()   
+    def __str__(self):
+        return self.name
