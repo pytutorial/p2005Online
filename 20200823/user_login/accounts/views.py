9ef0fcac8ebb9279b6e2a57406c7a6e83e2c5b68
Tung Duong <duongthanhtungvn01@gmail.com>
2020-08-23 09:10:56 +0100

    Update

--- 20200823/user_login/accounts/views.py
+++ 20200823/user_login/accounts/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+#from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserCreationForm
+#from django.contrib.auth.models import User
+from .models import CustomUser
 from django.contrib.auth import login, authenticate
 
 def signup(request):
-    form = UserCreationForm()
+    form = CustomUserCreationForm()
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             user = authenticate(username=user.username,
--- 20200823/user_login/user_login/settings.py
+++ 20200823/user_login/user_login/settings.py
@@ -122,3 +122,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
