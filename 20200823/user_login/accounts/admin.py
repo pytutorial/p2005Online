fd718d9e8614834f5404cc40423649ab39a34100
Tung Duong <duongthanhtungvn01@gmail.com>
2020-08-23 10:14:35 +0100

    Update

--- 20200823/user_login/accounts/admin.py
+++ 20200823/user_login/accounts/admin.py
@@ -1,3 +1,16 @@
-from django.contrib import admin
+#accounts/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import *
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'phone', 'email', 'address']
+    fieldsets = (*UserAdmin.fieldsets, 
+                    ('Thông tin khác', {
+                        'fields': ['phone', 'address']
+                    })
+                )
+
+admin.site.register(CustomUser, CustomUserAdmin)
