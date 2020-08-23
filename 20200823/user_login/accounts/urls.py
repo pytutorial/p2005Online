1c1e0993d1bfb477683d7c898f0a92e077dffba2
Tung Duong <duongthanhtungvn01@gmail.com>
2020-08-23 08:26:47 +0100

    Update

--- 20200823/user_login/accounts/urls.py
+++ 20200823/user_login/accounts/urls.py
@@ -1,7 +1,8 @@
 #accounts/urls.py
 
 from django.urls import include, path
-
+from .views import *
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
+    path('signup', signup),
 ]
