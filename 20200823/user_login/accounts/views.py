aeaa965ef0c8bedd0e7ac696304b3d952e873511
Tung Duong <duongthanhtungvn01@gmail.com>
2020-08-23 08:54:11 +0100

    Update

--- 20200823/user_login/accounts/views.py
+++ 20200823/user_login/accounts/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
 
-# Create your views here.
 def signup(request):
-    return render(request, 'registration/signup.html')
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user = authenticate(username=user.username,
+                    password=request.POST['password1'])
+            login(request, user)
+            return redirect('/')
+    
+    context = {'form': form}
+    return render(request, 'registration/signup.html', context)
--- 20200823/user_login/app/views.py
+++ 20200823/user_login/app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
 
 def index(request):
     return render(request, 'index.html')
 
-@login_required
+#@login_required
+@staff_member_required
 def staffView(request):
     return render(request, 'staff.html')
