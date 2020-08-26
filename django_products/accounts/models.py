7ad0a4fe92911c70bb3a0be091f138b144311a92
Tung Duong <duongthanhtungvn01@gmail.com>
2020-08-26 15:04:19 +0100

    Update

---
+++ django_products/accounts/models.py
@@ -0,0 +1,8 @@
+#accounts/models.py
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+class CustomUser(AbstractUser):
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=200)
---
+++ django_products/accounts/urls.py
@@ -0,0 +1,6 @@
+#accounts/urls.py
+from django.urls import path, include
+
+urlpatterns = [
+    path('', include('django.contrib.auth.urls'))
+]
