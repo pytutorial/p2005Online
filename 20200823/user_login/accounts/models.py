cc1717a98b97fcc4d71e6dc9766df92e50dbaf50
Tung Duong <duongthanhtungvn01@gmail.com>
2020-08-26 12:51:39 +0100

    Update

--- 20200823/user_login/accounts/models.py
+++ 20200823/user_login/accounts/models.py
@@ -3,6 +3,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class CustomUser(AbstractUser):
+class CustomUser(AbstractUser):    
     phone = models.CharField(max_length=20)
     address = models.CharField(max_length=200)
