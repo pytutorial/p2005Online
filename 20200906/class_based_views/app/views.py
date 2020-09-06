5dbb661a96a4ad06b691869967354350c19ac3e1
Tung Duong <duongthanhtungvn01@gmail.com>
2020-09-06 09:50:50 +0100

    Update

--- 20200906/class_based_views/app/views.py
+++ 20200906/class_based_views/app/views.py
@@ -1,13 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.views.generic.edit import CreateView, UpdateView
 from .models import Product
+from .forms import ProductForm
 #127.0.0.1:8000/create_product
 class ProductCreateView(CreateView):
-    model = Product
-    fields = '__all__'
-    template_name = 'product_form.html'
-    success_url = '/'
+    #model = Product
+    #fields = '__all__'
+    #template_name = 'product_form.html'
+    #success_url = '/'
+
+    def get(self, request):
+        form = ProductForm()
+        return render(request, 'product_form.html', {'form': form})
+
+    def post(self, request):
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')   
+        else:
+             return render(request, 'product_form.html', {'form':form})    
 
 # 127.0.0.1:8000/update_product/1
 class ProductUpdateView(UpdateView):
@@ -15,7 +28,6 @@ class ProductUpdateView(UpdateView):
     fields = '__all__'
     template_name = 'product_form.html'
     success_url = '/'
-
 
 # Create your views here.
 def index(request):
