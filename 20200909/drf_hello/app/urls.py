b3ea27975ce77b1cd03a910273181d4df524b51f
Tung Duong <duongthanhtungvn01@gmail.com>
2020-09-09 13:23:06 +0100

    Update

--- 20200909/drf_hello/app/urls.py
+++ 20200909/drf_hello/app/urls.py
@@ -3,4 +3,7 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('hello', hello),
+    path('search_product', searchProduct),
+    path('get_product_detail/<pk>', getProductDetail),
+    path('create_product', createProduct),
 ]
--- 20200909/drf_hello/app/views.py
+++ 20200909/drf_hello/app/views.py
@@ -5,3 +5,31 @@ from rest_framework.decorators import api_view
 @api_view(['GET'])
 def hello(request):
     return Response({'message': 'Hello'})
+
+productList = [
+    { 'id': 1, 'code': 'IPX', 'name': 'Iphone X'},
+    { 'id': 2, 'code': 'IP8', 'name': 'Iphone 8'},
+]    
+#127.0.0.1:8000/api/search_product?keyword=Iphone 8
+@api_view(['GET'])
+def searchProduct(request):
+    keyword = request.GET.get('keyword', '')
+    result = [p for p in productList if keyword in p['name']]
+    return Response(result)
+
+#127.0.0.1:8000/api/get_product_detail/1
+@api_view(['GET'])
+def getProductDetail(request, pk):
+    for p in productList:
+        if str(p['id']) == pk:
+            return Response(p)
+    return Response({'error': 'Not found'})
+#127.0.0.1:8000/api/create_product
+@api_view(['POST'])
+def createProduct(request):
+    data = request.data 
+    code = data.get('code')
+    name = data.get('name')
+    next_id = productList[-1]['id'] + 1 if productList else 1
+    productList.append({'id': next_id, 'code': code, 'name': name})
+    return Response({'success': True})
