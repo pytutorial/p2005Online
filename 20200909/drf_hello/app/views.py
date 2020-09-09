5080f43698809ae3b2c2949e18aa8fd8e290d7a2
Tung Duong <duongthanhtungvn01@gmail.com>
2020-09-09 13:40:01 +0100

    Update

--- 20200909/drf_hello/app/views.py
+++ 20200909/drf_hello/app/views.py
@@ -10,6 +10,7 @@ productList = [
     { 'id': 1, 'code': 'IPX', 'name': 'Iphone X'},
     { 'id': 2, 'code': 'IP8', 'name': 'Iphone 8'},
 ]    
+
 #127.0.0.1:8000/api/search_product?keyword=Iphone 8
 @api_view(['GET'])
 def searchProduct(request):
@@ -24,6 +25,7 @@ def getProductDetail(request, pk):
         if str(p['id']) == pk:
             return Response(p)
     return Response({'error': 'Not found'})
+
 #127.0.0.1:8000/api/create_product
 @api_view(['POST'])
 def createProduct(request):
@@ -33,6 +35,7 @@ def createProduct(request):
     next_id = productList[-1]['id'] + 1 if productList else 1
     productList.append({'id': next_id, 'code': code, 'name': name})
     return Response({'success': True})
+
 #127.0.0.1:8000/api/update_product/1
 @api_view(['PUT'])
 def updateProduct(request, pk):
