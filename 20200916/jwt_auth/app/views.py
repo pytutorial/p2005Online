7e76f3f64808725d0d2f6d2086b20fa6294767b3
Tung Duong <duongthanhtungvn01@gmail.com>
2020-09-16 13:28:05 +0100

    Update

--- 20200916/jwt_auth/app/views.py
+++ 20200916/jwt_auth/app/views.py
@@ -1,8 +1,14 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
-def hello(request):#127.0.0.1:8000/api/hello
+def hello(request):         #127.0.0.1:8000/api/hello
     return Response({'message': 'Hello'})
+
+class HelloView(APIView):   #127.0.0.1:8000/api/hello2
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        return Response({'message': 'Hello - 2'})    
