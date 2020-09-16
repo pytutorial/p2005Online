7cf826c658b22d80156532ca5a9c85b1791ef57b
Tung Duong <duongthanhtungvn01@gmail.com>
2020-09-16 13:21:49 +0100

    Update

--- 20200916/jwt_auth/app/views.py
+++ 20200916/jwt_auth/app/views.py
@@ -1,5 +1,8 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+
 @api_view(['GET'])
-def hello(request):
+@permission_classes([IsAuthenticated])
+def hello(request):#127.0.0.1:8000/api/hello
     return Response({'message': 'Hello'})
--- 20200916/jwt_auth/jwt_auth/settings.py
+++ 20200916/jwt_auth/jwt_auth/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -120,3 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+}
+
+REST_FRAMEWORK = {
+   'DEFAULT_AUTHENTICATION_CLASSES': [
+       'rest_framework_simplejwt.authentication.JWTAuthentication',
+   ],
+}
