f621191f9bbfb2cdf2e71320cae83c4c82144fa6
Tung Duong <duongthanhtungvn01@gmail.com>
2020-09-23 13:00:55 +0100

    Update

--- 20200923/viewset/app/urls.py
+++ 20200923/viewset/app/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from .views import *
+from rest_framework.routers import DefaultRouter
 urlpatterns = [
-    path('hello', hello),
+    path('hello', hello),    
 ]
+
+router = DefaultRouter()
+router.register('product', ProductViewSet)
+urlpatterns += router.urls
