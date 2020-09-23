e3575697ba9f90042bde24b42ddd5f36c12ac2aa
Tung Duong <duongthanhtungvn01@gmail.com>
2020-09-23 13:17:26 +0100

    Update

--- 20200923/viewset/app/urls.py
+++ 20200923/viewset/app/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from .views import *
 from rest_framework.routers import DefaultRouter
-urlpatterns = [
-    path('hello', hello),    
-]
-
+urlpatterns = [path('hello', hello),]
 router = DefaultRouter()
 router.register('product', ProductViewSet)
 urlpatterns += router.urls
+#127.0.0.1:8000/api/product : GET --> list, POST -> new
+#127.0.0.1:8000/api/product/1 : GET --> detail, PUT --> update, DELETE -->delete
--- 20200923/viewset/app/views.py
+++ 20200923/viewset/app/views.py
@@ -1,10 +1,23 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, action
 from rest_framework import viewsets
 from .serializers import *
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    #127.0.0.1:8000/api/product/search?keyword=IPhone
+    @action(detail=False, methods=['get'])
+    def search(self, request):
+        keyword = request.GET.get('keyword', '')
+        lst = Product.objects.filter(name__contains=keyword)
+        data = ProductSerializer(lst,many=True).data
+        return Response(data)
+
+    #127.0.0.1:8000/api/product/2/get_price
+    @action(detail=True, methods=['get'])
+    def get_price(self, request, pk):
+        p = Product.objects.get(pk=pk)
+        return Response({'price': p.price})
 
 
 @api_view(['GET'])
