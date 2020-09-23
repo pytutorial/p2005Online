d3ef4d15fee6471e0f336e178c10ea5ebbe7cd2d
Tung Duong <duongthanhtungvn01@gmail.com>
2020-09-23 12:57:05 +0100

    Update

--- 20200923/viewset/app/views.py
+++ 20200923/viewset/app/views.py
@@ -1,5 +1,12 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import viewsets
+from .serializers import *
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
 @api_view(['GET'])
 def hello(request):
     return Response({'message': 'Hello'})
